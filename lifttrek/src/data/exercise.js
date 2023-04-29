5b9335d99d1672366df26e56bfac58b9c06328ea
Nicholas Mule <nickmule77@gmail.com>
2023-04-29 14:40:15 -0400

    Auto Generated exercises function

--- lifttrek/src/data/exercise.js
+++ lifttrek/src/data/exercise.js
@@ -15,11 +15,11 @@ client.connect().then(() => {});
  * @returns Array of exercise objects, iterable with for of or map. The returned object is stored in redis under PagenumExercisetypeMuscleDifficulty. The storage type is a hashset, where each odd number is the name of the exercise and each even number is the corresponding object as a string
  */
 async function getExercises(pageNum, exerciseType = '', muscle = '', difficulty = ''){
-    if (!pageNum && pageNum != 0) throw "Error: Page number must be provided for exercise retrevial.";
-    if (pageNum < 0) throw "Error: Page number for exercise retrevial cannot be less than 0.";
-    if (exerciseType === "cardio") throw "Error: Cardio workouts currently not supported."
-    if (exerciseType === "plyometrics") throw "Error: Plyometric workouts currently not supported.";
-    if (exerciseType === "stretching") throw "Error: Stretching workouts currently not supported.";
+    if (!pageNum && pageNum != 0) throw "Error: Page number must be provided for exercise retrieval.";
+    if (pageNum < 0) throw "Error: Page number for exercise retrevial cannot be less than 0 for retrieval.";
+    if (exerciseType === "cardio") throw "Error: Cardio workouts currently not supported for retrieval."
+    if (exerciseType === "plyometrics") throw "Error: Plyometric workouts currently not supported for retrieval.";
+    if (exerciseType === "stretching") throw "Error: Stretching workouts currently not supported for retrieval.";
 
     let options = {
         method: 'GET',
@@ -37,6 +37,10 @@ async function getExercises(pageNum, exerciseType = '', muscle = '', difficulty 
         if (!inCache){
             let {data} = await axios.request(options);
             console.log("Storing exercises in cache as: " + combinedName);
+            if (data.length === 0){
+                //console.log("hi")
+                throw "Error: No exercise of those specifications exist, or they do not exist on this page.";
+            }
             for (const exercise of data){
                 await client.HSET(combinedName, exercise.name, JSON.stringify(exercise));
             }
@@ -61,11 +65,66 @@ async function getExercises(pageNum, exerciseType = '', muscle = '', difficulty 
     }
 }
 
+/**
+ * 
+ * @param {string} exerciseType exerciseType must be one of the possible catagories from exercises API. Cardio, stretching, and plyometrics not supported. Make sure it is lowercase.
+ * @param {Array<string>} musclesArray Array of muscles for one random workout to be selected for each.
+ * @param {string} difficulty Difficulty must be possible options from the exercises API. Make sure it is lowercase.
+ * @return Array of exercise objects, with one random exercise for each given muscle in the musclesArray, following the other filters.
+ */
+async function getExercisesAuto(exerciseType='', musclesArray, difficulty=''){ //use getExercises for a specified number of exercises; Then from that returned array, pick a random number from 1-10, and push array[randNum] to answerArray. return array at the end. do not store result in redis, it is random.
+    if (!musclesArray) throw "Error: Array of muscles to target must be given for auto-generation.";
+    if (exerciseType === "cardio") throw "Error: Cardio workouts currently not supported for auto-generation."
+    if (exerciseType === "plyometrics") throw "Error: Plyometric workouts currently not supported for auto-generation.";
+    if (exerciseType === "stretching") throw "Error: Stretching workouts currently not supported for auto-generation.";
+    if (!Array.isArray(musclesArray)) throw "Error: musclesArray must be an array for auto-generation.";
+
+    let rand1;
+    let rand2;
+    let pageNumMax = 100;
+    let currentPage;
+    let answerArray= [];
+    for (let i = 0; i < musclesArray.length; i++){
+        rand1 = Math.floor(Math.random() * pageNumMax); //random page from first 100 pages
+        rand2 = Math.floor(Math.random() * 10);
+
+        try {
+            currentPage = await getExercises(rand1, exerciseType, musclesArray[i], difficulty);
+        } catch (e) {
+            if (e === "Error: No exercise of those specifications exist, or they do not exist on this page."){
+                //console.log("actually working")
+                pageNumMax = rand1 - 1; //reduce possible range
+                i -= 1; 
+                continue; //effectively reset current iteration
+            }
+            else {
+                throw e;
+            }
+        }
+
+        //console.log(currentPage[rand2]);
+
+        if (currentPage[rand2]){
+            answerArray.push(currentPage[rand2]);
+        } 
+        else { //if random number does not exist, simply take last exercise of the current page
+            answerArray.push(currentPage[currentPage.length-1])
+        }
+        //console.log("current array length:" + answerArray.length)
+    }
+
+    return answerArray;
+}
+
+async function createWorkout(){
+
+}
+
 //for testing
 
 // async function main(){
 //     try {
-//         let exercises = await getExercises(0, 'strength', 'biceps', 'beginner');
+//         let exercises = await getExercisesAuto('strength', ['biceps'], 'beginner');
 //         console.log(exercises);
 //     } catch (e){
 //         console.log(e);
@@ -75,5 +134,6 @@ async function getExercises(pageNum, exerciseType = '', muscle = '', difficulty 
 // main();
 
 module.exports = {
-    getExercises
+    getExercises,
+    createWorkout
 }
