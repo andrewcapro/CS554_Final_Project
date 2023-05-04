26c297ff5aefd8f9af9890bc7db2c2cff383fde0
Haydn Dotterer <hdott>
2023-05-04 04:09:09 -0400

    Tweaked sign up & sign out

--- lifttrek/src/components/PrivateRoute.js
+++ lifttrek/src/components/PrivateRoute.js
@@ -4,7 +4,7 @@ import {AuthContext} from '../firebase/Auth';
 
 const PrivateRoute = () => {
   const {currentUser} = useContext(AuthContext);
-  console.log(currentUser);
+  //console.log(currentUser);
   //console.log('Private Route Comp current user', currentUser);
   // If authorized, return an outlet that will render child elements
   // If not, return element that will navigate to login page
