04260cfb31bb9040aa2bb9845a5dd8347e8ecc08
Andrew Capro <acapro@stevens.edu>
2023-05-08 02:35:27 -0400

    added description to home

--- lifttrek/src/components/Home.js
+++ lifttrek/src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <h2>Welcome to LiftTrek</h2>
-      <p>Description, someone write something please</p>
+      <p>What is LiftTrek about? Ever wanted to start working out, but did not know where to start? With countless muscles on the human body, and even more possible exercises, the options can be overwhelming and result in demotivation. With LiftTrek, you would be able to choose from a variety of muscles or muscle groups with a specific goal, and create a workout plan! At the same time, users can track their progress by uploading pictures after their workouts (with basic editing capabilities). Functioning as a form of social media, users can view exercise plans of other users and relevant images, leaving comments, likes, or adding the workout to their favorites.</p>
     </div>
   )
 }
